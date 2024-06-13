954e658d23e7b22e12dbcba7558dd180fd8e0f61
nick <nick@byniko.com>
2024-06-12 17:23:46 -0700

    about us page

--- dist/main.asset.php
+++ dist/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '365267ab7a707cc73034');
+<?php return array('dependencies' => array(), 'version' => '209413b782b5f43a9660');
--- page-about-us.php
+++ page-about-us.php
@@ -28,28 +28,30 @@ get_header();
 		</div>
 	</section>
 	<style>
-			.give_take {
-				position:relative;
-				overflow: hidden;
-			}
-			.bg_ampersand {
-				position:absolute;
-				left: 50%;
-				transform: translate(-50%, -50%);
-				font-size: 21vw;
-    line-height: 1;
-    font-weight: 800;
-    top: 50%;
-	opacity: .5;
-			}
-			.give_take .content {
-				text-align: center;
-				max-width: 300px;
-				padding: 1rem;
-			}
-		</style>
+		.give_take {
+			position: relative;
+			overflow: hidden;
+		}
+
+		.bg_ampersand {
+			position: absolute;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			font-size: 21vw;
+			line-height: 1;
+			font-weight: 800;
+			top: 50%;
+			opacity: .5;
+		}
+
+		.give_take .content {
+			text-align: center;
+			max-width: 300px;
+			padding: 1rem;
+		}
+	</style>
 	<section class="give_take theme--dark-gradient-1">
-		
+
 		<div class="bg_ampersand">&</div>
 		<div class="container--narrow">
 			<div class="flex-row justify--center">
@@ -64,64 +66,104 @@ get_header();
 			</div>
 		</div>
 	</section>
-	<section id="mapconnected-network" style="background-image: url('/src/images/mapconnected-network.jpg'); background-size:cover;">
-		<div class="container--very-narrow ">
-			
+	<section id="mapconnected-network" class="mapconnected-network">
+		<div class="inner-container">
+			<div class="container--very-narrow ">
+				<h2 class="h2">MAPconnected Network</h2>
+				<p>Our community and live forum connects people to a dynamic, information sharing network - providing unparalleled opportunities to understand relative performance and help make more efficient and informed warranty service lifecycle decisions.</p>
+			</div>
+			<div class="container--very-narrow">
+				<div class="flex-row __2x justify--center">
+					<div class="content">
+						<h3 class="h3">Open Collaboration</h3>
+						<p>We balance open communication with corporate sharing guidelines; encourage diverse perspectives, and actively connect members for collaboration.</p>
+					</div>
+					<div class="content">
+						<h3 class="h3">Trusted Community</h3>
+						<p>Members are connected by shared values and a commitment to mutual respect. Trust is built through consistent actions and open communication. </p>
+					</div>
+					<div class="content">
+						<h3 class="h3">Continuous Learning</h3>
+						<p>We strive to remain at the forefront of stakeholder concerns, leading-edge processes, and technological advancements to cultivate a vibrant learning environment by leveraging our robust platforms for knowledge exchange.</p>
+					</div>
+					<div class="content">
+						<h3 class="h3">Actionable Insights</h3>
+						<p>Crafted by our members, our event and survey agendas are tailored to deliver insights that are precise and relevant empowering our community to implement impactful actions that yield swift and tangible results. </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="theme--light-1">
+		<div class="container--very-narrow">
+			<div class="flex-column">
+				<h2 class="h2 text-center">Join to leverage these benefits and drive your business forward. </h2>
+				<div class="flex-row justify--center">
+					<a href="" class="button button--primary">Members Network</a>
+					<a href="" class="button button--primary button--outline">Annual Summit</a>
+					<a href="" class="button button--secondary">MyWarrantyNetwork Hub</a>
+				</div>
+			</div>
 		</div>
 		<div class="container--very-narrow">
-			<div class="content">
-				<p class="fz-md">We are soliciting your stories that share and validate hard won successes and innovative warranty and service operations insights.</p>
-			</div>
-			<div class="flex-row __2x justify--center">
-				<div class="d-flex"><a href="" class="button button--primary">Apply to Speak</a></div>
-				<div class="d-flex"><a href="" class="button button--primary">Apply to Sponsor</a></div>
-			</div>
-		</div>
-	</section>
-	<section>
-		<div class="container--fluid">
-			<div class="testimonial-slider">
-				<?php get_template_part('/acf-flex-starter/templates/testimonial-slider', null, array()); ?>
+			<div class="flex-column">
+				<h2 class="h2 text-center">Our Guiding Principals</h2>
+				<ul class="bullet-list fz-md">
+					<li>Unrivaled networking and best practice sharing platform</li>
+					<li>Unbiased - 100% neutral environment.</li>
+					<li>Members contribute to the program and topics.</li>
+					<li>Members follow the code of business conduct and ethics.</li>
+					<li>Receive new insights and inspiration by exchanging experiences and ideas between peers</li>
+				</ul>
 			</div>
 		</div>
 	</section>
+
 	<section class="theme--medium-1">
 		<div class="container--narrow">
-			<header class="h2 text-center">Thought Leadership At It’s Best! </header>
-			<div class="flex-row __4x">
-				<?php
-				load_class("Speaker");
-				$speakers = new Speaker();
-				$speakers = $speakers->get_speakers();
-				foreach ($speakers as $sp) :
-					$s = new Speaker($sp);
-				?>
-					<div class="flex-column">
-						<div class="speaker__logo text-center">
-							<?= $s->get_logo(); ?>
-						</div>
-						<div class="speaker__headshot">
-							<?= $s->get_headshot1(); ?>
-						</div>
-						<div class="speaker__meta">
-							<div class="speaker__name">
-								<?= $s->get_name(); ?>
-							</div>
-							<div class="speaker__title fz-xs">
-								<?= $s->get_title(); ?>
-							</div>
-						</div>
-					</div>
-				<?php endforeach; 	?>
+			<header class="text-center">
+				<h2 class="h2">Meet MAPconnected!</h2>
+				<p class="subtitle">Passionate Leaders Building a Connected Future</p>
+			</header>
+		</div>
+		<div class="container--single-column">
+			<div class="content">
+				<div class="fz-lg">MAPconnected is driven by a team dedicated to making your life in the Motor Vehicle Warranty and Aftercare Industry easier. Here's a glimpse into the minds behind the network:</div>
 			</div>
 		</div>
-	</section>
-	<section id="speaker-form" class="theme--dark-gradient-1">
-		<div class="container--very-narrow justify--center align-center">
-			<header class="text-center">
-				<h2 class="h2">Become a Speaker </h2>
-			</header>
-			<?php echo FrmFormsController::get_form_shortcode(array('id' => 4)); ?>
+		<div class="container--very-narrow">
+			<div class="names_and_roles flex-column">
+				<div class="name_role">
+					<h3 class="h3 name">
+						Pam Walter (Founder) :
+					</h3>
+					<div class="role fz-md">
+						<p>Pam's vision is at the heart of MAPconnected. As the founder, her passion lies in connecting people and fostering collaboration within the industry. With a strong belief in the power of shared knowledge and expertise, Pam has built a platform that empowers professionals to navigate today's challenges and achieve success.</p>
+					</div>
+				</div>
+				<div class="name_role">
+					<h3 class="h3 name">
+						Our Industry Experts:
+					</h3>
+					<div class="role fz-md">
+						<p>Pam is joined by a team of seasoned professionals in the motor vehicle warranty and aftercare industry. Their extensive experience provides invaluable insights that shape MAPconnected's offerings.
+							These experts collaborate with Pam to:</p>
+						<ul class="bullet-list">
+							<li><strong>Stay Abreast of Industry Trends:</strong> They ensure MAPconnected is at the forefront of industry developments, keeping members informed and equipped.</li>
+							<li><strong>Develop Valuable Resources:</strong> Their expertise is instrumental in creating insightful sessions, workshops, and resources that directly address current industry needs.</li>
+							<li><strong>Facilitate Meaningful Connections:</strong> They play a vital role in fostering connections between members, creating a collaborative environment for knowledge sharing and problem-solving.</li>
+						</ul>
+					</div>
+				</div>
+				<div class="name_role">
+					<h3 class="h3 name">
+						A Collaborative Spirit
+					</h3>
+					<div class="role fz-md">
+						<p>Together, Pam and the team at MAPconnected are committed to building a strong and supportive network for professionals in the motor vehicle warranty and aftercare industry.</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 
