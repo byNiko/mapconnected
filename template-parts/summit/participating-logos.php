d7832b789cb0a925c8a7002fbb4855fbeccb4984
nick <nick@byniko.com>
2024-07-12 17:57:24 -0700

    update participating logos

--- template-parts/summit/participating-logos.php
+++ template-parts/summit/participating-logos.php
@@ -2,7 +2,7 @@
 
 if (have_rows('attending_companies_group')) :
 	while (have_rows('attending_companies_group')) : the_row('attending_companies_group');
-		if ($logos = get_field('attending_company_logos')) :
+		if ($logos = get_sub_field('attending_company_logos')) :		
 ?>
 			<section id="participating-logos" class="theme--medium-1 py-1">
 				<div class="container--narrow">
@@ -11,7 +11,11 @@ if (have_rows('attending_companies_group')) :
 					</header>
 					<div class="logo-gallery">
 						<?php
+						$count = 0;
 						foreach ($logos as $logo) :
+							$count++;
+							$limit = get_sub_field('limit');
+							if ($limit < $count) break;
 						?>
 							<div class="logo-item"><?= wp_get_attachment_image($logo, 'medium', false, ['loading' => 'lazy']); ?></div>
 						<?php
