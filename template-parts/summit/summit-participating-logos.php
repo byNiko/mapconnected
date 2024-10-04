16c42784d49bc6305974c2a6c93c37d53be25615
nick <nick@byniko.com>
2024-10-04 16:26:25 -0700

    update past attendees boolean

--- template-parts/summit/summit-participating-logos.php
+++ template-parts/summit/summit-participating-logos.php
@@ -4,7 +4,7 @@ if ($attending_group = get_field('attending_companies_group')) :
 
 	$show_current = $attending_group['show_current_or_past_attenddees'];
 
-	$attending_logos = $show_current ? get_field('past_attendees_logos', 'options') : $attending_group['attending_company_logos'];
+	$attending_logos = $show_current ?  $attending_group['attending_company_logos']: get_field('past_attendees_logos', 'options') ;
 	if (count($attending_logos) > 0) :
 		$logo_limit = $attending_group['limit'];
 		$title = $attending_group['title'];
