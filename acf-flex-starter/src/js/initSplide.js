23c561f1f1bb12150a0761c7c944bf0f205a1983
nick <nick@byniko.com>
2025-03-28 11:42:03 -0700

    back to normal

--- acf-flex-starter/src/js/initSplide.js
+++ acf-flex-starter/src/js/initSplide.js
@@ -7,24 +7,24 @@ export function initSplide() {
 		slide.parentElement.parentElement.style.height = slide.offsetHeight + 'px';
 	};
 
-	// document.addEventListener( 'load', function() {
-	// Query all sliders
-	const splideElements = document.querySelectorAll( '.splide' );
+	document.addEventListener( 'DOMContentLoaded', function() {
+		// Query all sliders
+		const splideElements = document.querySelectorAll( '.splide' );
 
-	// Initialize each slider instance
-	splideElements.forEach( function( el ) {
-		// Retrieve the options from the data-splide attribute
-		const options = el.dataset.splide ? JSON.parse( el.dataset.splide ) : {};
-		const slider = new Splide( el, options );
-		// Initialize slider with the options
-		const extensions = options.autoScroll 	? window.splide.Extensions : null;
-		slider.mount( extensions );
-		if ( options.adaptiveHeight ) {
-			slider.on( 'resized move ', () => {
+		// Initialize each slider instance
+		splideElements.forEach( function( el ) {
+			// Retrieve the options from the data-splide attribute
+			const options = el.dataset.splide ? JSON.parse( el.dataset.splide ) : {};
+			const slider = new Splide( el, options );
+			// Initialize slider with the options
+			const extensions = options.autoScroll 	? window.splide.Extensions : null;
+			slider.mount( extensions );
+			if ( options.adaptiveHeight ) {
+				slider.on( 'resized move ', () => {
+					updateHeight( slider );
+				} );
 				updateHeight( slider );
-			} );
-			updateHeight( slider );
-		}
+			}
+		} );
 	} );
-	// } );
 }
