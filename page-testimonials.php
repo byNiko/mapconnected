8f5772d25ac6a82d2e89d2579bae9330464b4b34
nick <nick@byniko.com>
2025-07-04 05:26:48 -0700

    fix testimonials

--- page-testimonials.php
+++ page-testimonials.php
@@ -47,48 +47,142 @@ shuffle($content_items);
 ?>
 <main id="primary" class="site-main">
 	<style>
-		.testimonial-image-grid {
-			display: flex;
-			flex-wrap: wrap;
-			gap: 1rem;
-			& > * {
-				width: 30%;
+		.masonry-gallery {
+			position: relative;
+		}
+
+		.grid-item {
+			position: absolute;
+			padding: 1rem;
+			width: 31.4%;
+			background: orange;
+
+			@media (max-width: 768px) {
+				width: 100%;
+				position: initial;
+				margin-bottom: 1rem;
+
+				&:last-of-type {
+					margin-bottom: 0;
+				}
 			}
-			img {
-				width: 50%;
-				object-fit:cover;
+
+			h3 {
+				margin: 0 0 1rem;
+				border-bottom: 1px solid white;
+				text-align: left;
+				font-size: 24px;
 			}
 		}
 	</style>
 	<div class="container">
 		<?php
-		while (have_posts()) :
+		if (have_posts()) :
 			the_post();
 			the_title("<h1 class='page-title'>", "</h1>");
 			get_template_part('template-parts/content', 'page');
 			if (! empty($content_items)) : ?>
-				<div class="testimonial-image-grid">
+				<div id="masonry-gallery" class="masonry-gallery ">
+					<div class="grid-sizer"></div>
+					<div class="grid-item"></div>
 					<?php
-					foreach ($content_items as $item) {
+					$width_classes = [
+						'grid-item--width2',
+						'grid-item--width3',
+						'grid-item--width4',
+					];
+					$height_classes = [
+						'grid-item--height2',
+						'grid-item--height3',
+						'grid-item--height4',
+					];
+					foreach ($content_items as $item) :
+						$rand_height = array_rand($height_classes, 1);
+						$rand_width = array_rand($width_classes, 1);
 						if (is_a($item, 'Testimonial')) {
-							echo $t->get_single_testimonial_html();
+					?>
+							<div class="grid-item <?= $height_classes[$rand_width]; ?> <?= $height_classes[$rand_height]; ?>">
+								<?php
+								echo $t->get_single_testimonial_html();
+								?>
+							</div>
+						<?php
 						} else {
-							echo wp_get_attachment_image($item['id'], 'medium');
+						?>
+							<div class="grid-item <?= $height_classes[$rand_width]; ?> <?= $height_classes[$rand_height]; ?>">
+								<?php
+								echo wp_get_attachment_image($item['id'], 'full');
+								?>
+							</div>
+					<?php
 						}
-					}
+					endforeach;
+
 					?>
 				</div>
 		<?php
 			endif;
-		// If comments are open or we have at least one comment, load up the comment template.
-		// if (comments_open() || get_comments_number()) :
-		// 	comments_template();
-		// endif;
-
-		endwhile; // End of the loop.
+		endif; // End of the loop.
 		?>
 	</div>
 </main><!-- #main -->
 <?php //get_sidebar('sidebar'); 
 ?>
 <?php get_footer(); ?>
+<script src="https://unpkg.com/imagesloaded@5/imagesloaded.pkgd.js"></script>
+<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+<script>
+	var colCount = 0;
+	var colWidth = 0;
+	var windowWidth = 0;
+	var blocks = [];
+	const $ = jQuery;
+
+	function positionBlocks() {
+
+		$('.grid-item').each(function() {
+			var min = Array.min(blocks),
+				index = $.inArray(min, blocks),
+				leftPos = margin + (index * (colWidth + margin));
+			$(this).css({
+				'left': leftPos + 'px',
+				'top': min + 'px'
+			});
+			blocks[index] = min + $(this).outerHeight() + margin;
+		});
+	}
+
+	function setupBlocks() {
+		windowWidth = $('.masonry-gallery').width();
+		console.log(windowWidth);
+		if (windowWidth <= 768) {
+			return;
+		}
+		colWidth = $('.grid-item').outerWidth();
+		margin = (windowWidth - colWidth * 3) / 4;
+		blocks = [];
+		colCount = Math.floor(windowWidth / (colWidth + margin));
+		for (var i = 0; i < colCount; i++) {
+			blocks.push(margin);
+		}
+		positionBlocks();
+		$('.masonry-gallery').height(Array.max(blocks));
+	}
+
+	$(function() {
+		$(window).resize(setupBlocks);
+	});
+
+	// Function to get the Min value in Array
+	Array.min = function(array) {
+		return Math.min.apply(Math, array);
+	};
+	// Function to get the Max value in Array
+	Array.max = function(array) {
+		return Math.max.apply(Math, array);
+	};
+
+	$(document).ready(function() {
+		setupBlocks();
+	});
+</script>
